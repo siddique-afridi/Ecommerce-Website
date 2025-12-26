17b037489c403080836ec9962e8673898eea4037
Siddique Afridi <siddiqueafridigfx@gmail.com>
2025-12-26 11:33:31 +0500

    created product component to show details of selected product

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext, useEffect,useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);     //false acts like placeholder telling react i had nothing right now to render
+  const [image, setImage] = useState('')
+
+  const fetchProductData = async()=> {
+
+    products.map((item)=>{
+      if(item._id === productId){
+        setProductData(item) 
+        setImage(item.image[0])                               //here false is replaced bcz now we have data to render
+        console.log(item)
+        return null
+      }
+    } )
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[productId,products])
+
+
+    return productData? (
+    <div className='border-t-2 pt-10 transition-opacity-0 ease-in duration-500 opacity-100'>
+      {/* product data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+        {/* image of prod */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row '>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item,i)=> (
+                <img onClick={()=> setImage(item)} src={item} key={i} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img src={image} className='w-full h-auto' alt="" />
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+  ) : <div className='opacity-0'></div>
+
 }
 
 export default Product
