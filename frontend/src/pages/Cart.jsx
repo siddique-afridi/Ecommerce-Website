ede1d44ecc9432fa20e23590a16cb9ab084f3029
Siddique Afridi <siddiqueafridigfx@gmail.com>
2025-12-31 12:01:25 +0500

    added Cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
 
-export default Cart
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="pt-14 border-t">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+
+      <div>
+        {cartData.map((item, i) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+          return (
+            <div
+              key={i}
+              className="py-4 border-t grid grid-cols-[4fr_1fr_1fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="flex gap-6 items-start">
+                <img
+                  src={productData.image[0]}
+                  className="w-16 sm:w-20"
+                  alt=""
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">
+                    {productData.name}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="px-2 sm:px-2 bg-slate-50 border sm:py-0">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <input className="border max-w-10 sm:max-w-20 sm:px-2 px-1 py-1 " type="number" min={1} defaultValue={item.quantity} />
+              <img src={assets.bin_icon} className="w-4 mr-4 sm:w-5 cursor-pointer" alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
