f8889b3ef4a8d843fa350198a24e38e5db700f5a
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-01-13 11:31:18 +0500

    completed Orders page UI,making login page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -49,15 +49,15 @@ const PlaceOrder = () => {
 
           {/* Payment method selection */}
           <div className='flex flex-col gap-3 lg:flex-row'>
-                <div onClick={()=>setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <div onClick={()=>setMethod('stripe')} className='flex items-center gap-0 border p-2 px-3 cursor-pointer'>
                   <p className={`min-w-3.5 h-3.5 border rounded-full ${method === "stripe" ? 'bg-green-400' : ''}`}></p>
                   <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
                 </div>
-                <div onClick={()=>setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <div onClick={()=>setMethod('razorpay')} className='flex items-center gap-0 border p-2 px-3 cursor-pointer'>
                   <p className={`min-w-3.5 h-3.5 border rounded-full ${method === "razorpay" ? 'bg-green-400' : ''}`}></p>
                   <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
                 </div>
-                <div onClick={()=>setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <div onClick={()=>setMethod('cod')} className='flex items-center gap-0 border p-2 px-3 cursor-pointer'>
                   <p className={`min-w-3.5 h-3.5 border rounded-full ${method === "cod" ? 'bg-green-400' : ''}`}></p>
                   <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
                 </div>
