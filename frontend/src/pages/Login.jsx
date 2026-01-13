e6a4e884bd4a28af05a5a6148db04b5728e115be
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-01-13 12:15:26 +0500

    completed Login/Signup page, single page used for both operations using ternary operators

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign Up')
+
+  const onSubmitHandler=async(e)=> {
+    e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-700'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+      </div>
+      {currentState === 'Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required /> }
+      <input type="email" className='w-full px-3 py-2 border border-gray-800'  placeholder='Email' required/>
+      <input type="password" className='w-full px-3 py-2 border border-gray-800'  placeholder='Password'  required/>
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+        <p className='cursor-pointer  hover:text-blue-500'>Forgot Your Password?</p>
+        {
+          currentState === 'Login' 
+          ? <p onClick={()=> setCurrentState('Sign Up')} className='cursor-pointer hover:text-blue-500'>Create Account</p>
+          : <p onClick={()=> setCurrentState('Login')} className='cursor-pointer hover:text-blue-500'>Login Here</p>
+        }
+        
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4 hover:bg-orange-500 ease-in-out duration-300 outline-none hover:rounded-lg'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+
+    </form>
   )
 }
 
