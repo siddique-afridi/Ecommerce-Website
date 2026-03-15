274818efb6c1d2d84215a9b481b47ad426b4bee9
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-03-16 00:31:28 +0500

    rerouting fixed

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
+
+//App config
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+
+app.get('/', (req,res)=> {
+    const h = new Date().getHours();
+    const m = new Date().getMinutes();
+    const s = new Date().getSeconds();
+    res.send({message:"Backend is running", code: 200, time:`${h}:${m}:${s}` })
+})
+
+
+app.listen(port, ()=> console.log('Server running on port :' + port))
