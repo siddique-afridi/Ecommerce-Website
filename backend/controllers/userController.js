3bc8821cbb63f5d22f7678c22541be959d4c97cb
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-04-28 23:21:13 +0500

    user Model created, register, login and adminLogin controllers created and APIs tested

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+
+// login controller
+const loginUser = async(req,res)=> {
+res.json({msg:"login API working"})
+}
+
+// register controller
+const registerUser = async(req,res)=> {
+    res.json({msg:"register API working"})
+
+}
+
+// adminLogin controller
+const adminLogin = async(req,res)=> {
+
+}
+
+
+
+export {loginUser,registerUser,adminLogin}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/dbConfig.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 
 //App config
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
+app.use('/api/user', userRouter)
 
 app.get('/', (req,res)=> {
     const h = new Date().getHours();
