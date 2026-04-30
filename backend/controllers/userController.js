58efdbd83228f234155875b1e35fcf089f0935b1
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-04-30 18:50:38 +0500

    product controllers setted up, addProduct API is tested and working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,83 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
+
+const createToken = (id)=> {
+    return jwt.sign({id}, process.env.JWT_SECRET);
+}
 
 
 // login controller
-const loginUser = async(req,res)=> {
-res.json({msg:"login API working"})
-}
+const loginUser = async (req, res) => {
+
+      try{
+        const {email,password} = req.body;
+        const user = await userModel.findOne({email});
+
+        if (!user){
+              return res.json({success:false, message: "User doesn't exists"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(isMatch){
+            const token = createToken(user._id)
+            res.json({success:true, token})
+
+        }else{
+            res.json({success:false, message: "Invalid credentials"})
+        }
+
+
+      }catch(error){
+        console.log(error);
+        res.json({success:false, message:error.message})
+
+      }
+
+};
+
+
 
 // register controller
-const registerUser = async(req,res)=> {
-    res.json({msg:"register API working"})
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password:abcdefg } = req.body;
 
-}
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "user already exists" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({ succes: false, message: "Please enter a valid email" });
+    }
+    if(password.length<8){
+        return res.json({success:false, message: "Please enter a strong password"})
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+console.log(hashedPassword)
+    const newUser = new userModel({
+        name,
+        email,
+        password:hashedPassword
+    })
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    res.json({success:true, token})
+
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:error.message})
+  }
+};
 
 // adminLogin controller
-const adminLogin = async(req,res)=> {
+const adminLogin = async (req, res) => {};
 
-}
-
-
-
-export {loginUser,registerUser,adminLogin}
+export { loginUser, registerUser, adminLogin };
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/dbConfig.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 
 //App config
@@ -19,6 +20,8 @@ app.use(cors())
 
 //api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+
 
 app.get('/', (req,res)=> {
     const h = new Date().getHours();
