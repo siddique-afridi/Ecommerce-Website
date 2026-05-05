a4d887bac5b2b90e5fd298d5c3e135a57d0acc09
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-05-05 20:26:46 +0500

    authentication middleware created for admin

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,17 +2,18 @@
 import express from 'express'
 import { listProducts,singleProduct,addProduct,removeProduct } from '../controllers/productController.js'
 import upload from '../middlewares/multer.js';
+import adminAuth from '../middlewares/adminAuth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.fields([
+productRouter.post('/add',adminAuth,upload.fields([
         {name:'image1', maxCount:1 },
         {name:'image2', maxCount:1 },
         {name:'image3', maxCount:1 },
         {name:'image4', maxCount:1 },
     ]), addProduct)
 
-productRouter.post('/remove', removeProduct)
+productRouter.post('/remove',adminAuth, removeProduct)
 productRouter.post('/single', singleProduct)
 productRouter.get('/list', listProducts)
 
