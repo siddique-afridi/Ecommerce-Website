47b3ce393d4e9b9bd4a82986a16b07f4a239ee78
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-05-05 20:13:22 +0500

    adminLogin API working, next will create authentication middlewear

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,6 +78,25 @@ console.log(hashedPassword)
 };
 
 // adminLogin controller
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try{
+    const {email,password} = req.body;
+
+    if(email === process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+      
+      const token = jwt.sign(email+password, process.env.JWT_SECRET);
+      res.json({success:true, token})
+      console.log(token)
+
+    }else{
+      res.json({success:false, message: "invalid credentials"})
+    }
+
+  }catch(error){
+     console.log(error);
+    res.json({success:false, message:error.message})
+  }
+
+};
 
 export { loginUser, registerUser, adminLogin };
