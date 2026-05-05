877a97f6afba121e2cc2ae1c387de877e7cab975
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-05-05 23:35:14 +0500

    components,pages, routing setup done, basic layout of admin dashboard done. now next we add admin authentication

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Routes,Route } from 'react-router-dom'
+import Orders from '../src/pages/Orders'
+import List from './/pages/List'
+import Add from './/pages/Add'
 
 const App = () => {
   return (
-    <>
+    <div className='bg-gray-50 min-h-screen'>
+    
     <Navbar/>
+    <hr />
+    <div className='flex w-full'>
+
+    <Sidebar/>
+
+    <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+         <Routes>
+          <Route path='/add' element={<Add/>} />
+          <Route path='/list' element={<List/>} />
+          <Route path='/orders' element={<Orders/>} />
+         </Routes>
+
+    </div>
+
+    </div>
       
-    </>
+    </div>
   )
 }
 
