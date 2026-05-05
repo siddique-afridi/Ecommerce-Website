e68da316520c473787c552221a8b8659ead55e6f
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-05-05 23:08:23 +0500

    created admin folder in root directory and scaffolded react project in it.

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from "../assets/assets"
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-2 px-[4%]'>
+        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+        <button className='bg-gray-600 text-white px-5 py-2 sm:py-2 rounded-full tessxt-xs sm:text-sm'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
