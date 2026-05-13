928893fa665dbc382c801df6ae3780dd608b72b9
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-05-13 22:27:22 +0500

    order status logic created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -70,5 +70,16 @@ export const userOrders = async(req,res)=> {
 }
 
 export const updateStatus = async(req,res)=> {
+    try{
+        const {orderId, status} = req.body;
+
+        await orderModel.findByIdAndUpdate(orderId, {status})
+        res.json({success:true, message:"Status Updated"})
+
+    }catch(error){
+         console.log(error)
+        res.json({success:false, message:error.message})
+
+    }
 
 }
