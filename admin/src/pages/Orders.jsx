8886f60d7bbbdac89b1e14ceaee560ac1d2b8ced
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-05-13 22:28:58 +0500

    Orders page completed with all features, status update is also working, now we will integrate payment gateways

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -31,6 +31,24 @@ const Orders = ({ token }) => {
     }
   };
 
+const statusHandler = async(event,orderId)=>{
+        try{
+          const res = await axios.post(backendUrl + 
+            '/api/order/status',
+             {orderId, status:event.target.value},
+             {headers:{token}})
+
+             if(res.data.success){
+              await fetchAllOrders()
+             }
+
+
+        }catch(error){
+                      console.log(error)
+                      toast.error(res.data.message)
+        }
+}
+
   useEffect(() => {
     fetchAllOrders();
   }, [token]);
@@ -85,7 +103,7 @@ const Orders = ({ token }) => {
             </div>
 
             <p className="text-sm sm:text-[15px]"> {currency}{order.amount} </p>
-            <select className="p-2 font-semibold">
+            <select onChange={(event)=>statusHandler(event, order._id)} value={order.status} className="p-2 font-semibold">
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
